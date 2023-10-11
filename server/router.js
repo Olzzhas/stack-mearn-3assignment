194014c4353db9778ac7578719c72af267abd29b
Olzzhas <olzhasayato@gmail.com>
2023-10-11 20:56:11 +0600

    some updates

--- server/router.js
+++ server/router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const User = require('./UserModel');
-const ObjectID = require('mongodb').ObjectId;
 const cors = require('cors');
 const router = new express();
 
@@ -8,18 +7,23 @@ router.use(cors());
 router.options('*', cors());
 
 router.post('/user-create', cors(), async (req, res) => {
-  const candidate = await User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  const candidate = await User.findOne({ email: req.body.email });
+  if (!candidate) {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-  res.send({ candidate });
+    res.send({ user });
+  } else {
+    res.json({ message: 'This email is already in use!' });
+  }
 });
 
 router.get('/user-getall', async (req, res) => {
   const result = await User.find({});
-  res.send({ users: result });
+  res.send(result);
 });
 
 router.get('/user-get', async (req, res) => {
@@ -27,6 +31,21 @@ router.get('/user-get', async (req, res) => {
 
   const result = await User.findOne({ email: email });
   res.send({ result });
+});
+
+router.post('/user-delete', cors(), async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user == null) {
+    res.json({ message: 'user is not found' });
+  } else {
+    await User.deleteOne().then((err) => {
+      if (err != null) {
+        res.send(err);
+      } else {
+        res.json({ message: 'user is deleted successfully' });
+      }
+    });
+  }
 });
 
 // router.get(`/user-get/:id`, async (req, res) => {
